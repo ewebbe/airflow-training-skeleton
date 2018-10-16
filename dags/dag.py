eda0969ed73d77275f1505ef3c404815a53a030a
ewebbe <ewebbe@bol.com>
2018-10-16 14:16:39 +0200

    adding build a cluster

--- dags/dag.py
+++ dags/dag.py
@@ -2,7 +2,7 @@ import datetime as dt
 import constants as c
 from airflow import DAG
 # from airflow.operators.python_operator import PythonOperator
-# from airflow.utils.trigger_rule import TriggerRule
+from airflow.utils.trigger_rule import TriggerRule
 from godatadriven.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 from airflow.contrib.operators.dataproc_operator import (
     DataprocClusterCreateOperator,
